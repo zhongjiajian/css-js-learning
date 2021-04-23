0ea5166235ea112479d39c14449b67e2b59a8a7a
钟家建 <zhongjiajian@baofeng.com>
2021-04-23 19:31:31 +0800

    script

---
+++ addPage.js
@@ -0,0 +1,51 @@
+const { rejects } = require("assert");
+var {
+    exec
+} = require("child_process");
+var fs = require('fs');
+const { resolve } = require("path");
+var path = require('path');
+
+var args = process.argv.splice(2);
+
+function checkOptions(args) {
+    return new Promise((resolve, reject) => {
+        if (!args.includes('-n')) {
+            return reject("缺少n参数,like: node addPage.js -n '新页面名字'");
+        };
+        let fileName = args[args.indexOf('-n') + 1];
+        return resolve(fileName);
+    })
+}
+
+function writeFile(fileName){
+    return new Promise((resolve,reject)=>{
+        const filePath = path.resolve(__dirname,`./demos/${fileName}.html`)
+        let content = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${fileName}</title>
+    <style></style>
+</head>
+<body>
+    
+</body>
+</html>`;
+        
+        fs.writeFile(filePath,content,err=>{
+            if(err) return reject(err);
+            resolve("add success");
+        })
+    })
+}
+
+(async function () {
+    const fileName = await checkOptions(args);
+    return await writeFile(fileName);
+})()
+.then(console.log)
+.catch(console.log)
+
